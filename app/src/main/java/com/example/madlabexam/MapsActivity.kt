bae102256168c87d8f302491df8d61c9a0854ad4
Amir Ghafoor <77319741+meamirghafoor@users.noreply.github.com>
2022-06-10 15:33:27 +0500

    git desktop

---
+++ app/src/main/java/com/example/madlabexam/MapsActivity.kt
@@ -0,0 +1,28 @@
+package com.example.madlabexam
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.madlabexam.databinding.ActivityMapsBinding
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val riphah = LatLng(31.378, 74.2319);
+        mMap.addMarker(MarkerOptions().position(riphah).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(riphah))
+    }
+}
